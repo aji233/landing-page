cf80f1421972226a016fc6b83b94c63c631b8207
seagleax <litle.project@gmail.com>
2024-09-08 21:07:06 +0700

    move to main folder

--- company-profile/model-1/assets/core.js
+++
@@ -1,9 +0,0 @@
-const toggle = () => {
-    const open = localStorage.getItem('menu')
-    const status = open === 'open' ? 'close' : 'open'
-    localStorage.setItem('menu', status)
-
-    const menu = document.getElementById('menu').classList
-    if (status === 'close') menu.add('hidden')
-    else menu.remove('hidden')
-}
